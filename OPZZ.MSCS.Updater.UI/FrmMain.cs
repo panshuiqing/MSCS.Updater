893c53650be52426f59f81c3e401dfbbfc990717
panshuiqing <panshuiqing2007@qq.com>
2019-07-24 17:11:51 +0800

    dd

--- OPZZ.MSCS.Updater.UI/FrmMain.cs
+++ OPZZ.MSCS.Updater.UI/FrmMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -8,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OPZZ.MSCS.Updater.Core;
+using SharpSvn;
+using SharpSvn.Security;
 
 namespace OPZZ.MSCS.Updater.UI
 {
@@ -216,8 +219,6 @@ namespace OPZZ.MSCS.Updater.UI
 
         private void BtnSvnLog_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("功能开发中，敬请期待...");
-            return;
 
             if (tvServers.SelectedNode == null)
             {
@@ -225,17 +226,123 @@ namespace OPZZ.MSCS.Updater.UI
                 return;
             }
 
-            var nodeData = tvServers.SelectedNode.Tag as ServerConfig;
-            if (nodeData != null)
+            try
             {
-                var lastUpdateDate = UpdateHistory.GetLatest(nodeData.Id).PublishAt;
+                var nodeData = tvServers.SelectedNode.Tag as ServerConfig;
+                if (nodeData != null)
+                {
+                    //var lastUpdateDate = UpdateHistory.GetLatest(nodeData.Id).PublishAt;
+                    var files = GetSVNFiles(DateTime.Now.AddDays(-1), "https://120.77.204.102/svn/ZZXXCode/trunk/MSCS");
+                }
+                else
+                {
+                    MessageBox.Show("请选择发版服务器");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("获取SVN文件异常："+ex.Message);
+            }
+        }
 
-            }
-            else
+        private IEnumerable<SvnPathInfo> GetSVNFiles(DateTime lastDate, string svnPath)
+        {
+            IEnumerable<SvnPathInfo> files = new SvnPathInfo[0];
+
+            Uri uriBase = new Uri(svnPath);
+            DateTime dtBeg = lastDate.ToUniversalTime();
+            DateTime dtEnd = DateTime.Now.AddMinutes(5).ToUniversalTime();
+            SvnRevisionRange range = new SvnRevisionRange(dtBeg, dtEnd);
+            SvnLogArgs logArgs = new SvnLogArgs(range);
+            logArgs.BaseUri = uriBase;
+            SvnClient client = new SvnClient();
+            client.Authentication.UserNamePasswordHandlers += new EventHandler<SvnUserNamePasswordEventArgs>(Authentication_UserNamePasswordHandlers);
+
+            Collection<SvnLogEventArgs> lstLogEvent = new Collection<SvnLogEventArgs>();
+            if (client.GetLog(uriBase, logArgs, out lstLogEvent))
             {
-                MessageBox.Show("请选择发版服务器");
-                return;
+                Dictionary<string, SvnPathInfo> lstFiles = new Dictionary<string, SvnPathInfo>();
+                SvnPathInfo pathInfo = null;
+                foreach (SvnLogEventArgs log in lstLogEvent)
+                {
+                    foreach (SvnChangeItem item in log.ChangedPaths)
+                    {
+                        if (item.NodeKind == SvnNodeKind.File)
+                        {
+                            if (ContainsExclude(item.Path))
+                            {
+                                continue;
+                            }
+
+                            if (lstFiles.ContainsKey(item.Path))
+                            {
+                                pathInfo = lstFiles[item.Path];
+                            }
+                            else
+                            {
+                                pathInfo = new SvnPathInfo();
+                                pathInfo.FilePath = item.Path;
+                                pathInfo.Author = log.Author;
+                                lstFiles[item.Path] = pathInfo;
+                            }
+
+                            if (log.LogMessage != null && log.LogMessage.Trim().Length > 0)
+                            {
+                                string[] messages = log.LogMessage.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                                foreach (var msg in messages)
+                                {
+                                    if (string.IsNullOrWhiteSpace(msg))
+                                        continue;
+                                    pathInfo.Log.Append(msg + ";");
+                                }
+                            }
+                        }
+                    }
+                }
+
+                files = lstFiles.Values.ToArray();
             }
+
+            return files;
+        }
+
+        void Authentication_UserNamePasswordHandlers(object sender, SvnUserNamePasswordEventArgs e)
+        {
+            e.UserName = "mscsdev";
+            e.Password = "1";
+            e.Save = true;
+        }
+
+        private bool ContainsExclude(string path)
+        {
+            List<string> lstExclude = new List<string> { ".sln", ".csproj", ".licx" };
+            foreach (var ex in lstExclude)
+            {
+                if (path.EndsWith(ex))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class SvnPathInfo
+    {
+        public StringBuilder Log { get; set; }
+
+        public string FilePath { get; set; }
+
+        public string Author { get; set; }
+        public string Remark { get { return Log.ToString().TrimEnd(';').Trim(); } }
+        public string ResolvePath { get { return FilePath; } }
+        public SvnPathInfo()
+        {
+            Log = new StringBuilder();
+            Author = "";
+            FilePath = string.Empty;
         }
     }
 }
